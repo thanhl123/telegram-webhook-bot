509a7f9e2e73434233589ad699867fa0d04fe4cd
thanhl123 <ngoc.bin.1989@gmail.com>
2025-07-12 00:15:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import requests
 app = Flask(__name__)
 BOT_TOKEN = "7619429510:AAFhVN18rurOAgPQgLYo05BUXCF0N7R58Cg"
 
-@app.route('/', methods=['POST'])
+@app.route(f'/{BOT_TOKEN}', methods=['POST'])
 def webhook():
     data = request.json
     chat_id = data["message"]["chat"]["id"]
     message = data["message"].get("text", "Không có nội dung")
-    
+
     requests.post(
         f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
         json={"chat_id": chat_id, "text": f"Bạn gửi: {message}"}
